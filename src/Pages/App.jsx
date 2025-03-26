14f3540601e93947247fdd120fcc3b5335a716bd
SoTotallyMantas <mantastrojak@gmail.com>
2025-03-26 12:32:49 +0200

    ASD

--- src/Pages/App.jsx
+++ src/Pages/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
-
+import { NavLink } from 'react-router';
 import TaskList  from '../Components/TaskList'
 function App() {
   
@@ -10,12 +10,22 @@ function App() {
 
   return (
     <>
-          <main className="container mt-5 text-center">
-          <div className="row gx-5">
-              <h1>Task Management App</h1>
-                  <TaskList />
-          </div>
-          </main>
+
+              <main className="d-flex flex-column justify-content-center align-items-center min-vh-100">
+                <div className="card m-5 p-5">
+                    <div className="card-body">
+                        <h5 className="card-title text-center">Dashboard</h5>
+                        <NavLink to="/login" className="btn btn-primary m-3 w-100">
+                            Login
+                        </NavLink>
+                        <NavLink to="/register" className="btn btn-primary m-3 w-100">
+                            Register
+                        </NavLink>
+                    </div>
+                    
+                </div>
+            </main>
+        
     </>
   )
 }
